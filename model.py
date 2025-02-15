6b8eeb6c6ed1048501b56d2b529b4d4b22ee80b6
RcyuH <phunghuy.ms@gmail.com>
2025-02-15 15:06:13 +0700

    Pseudo Code Note

--- model.py
+++ model.py
@@ -6,3 +6,26 @@ Created on Fri Feb 14 13:31:03 2025
 @author: rcyuh
 """
 
+"""
+******Algo******
+
+B1: Tính semantic similiraty
+    +) Input: df chứa infor của items gồm title, description, 
+           category, brand, sales ranking, price.
+    +) Prompt vào LLM để nó cho ra embedding vector tương ứng
+    +) Tính cosine similirity cho đôi một item embedding vector 
+    +) Output: Ma trận Rs (size NxN, N là số items trong system)
+
+B2: Tính collaborative commonality
+    +) Input: User-Item matrix C, input đặc trưng của bài toán đề xuất
+              Giá trị trong đó có thể là ratings, number of clicks,... (Chưa thấy đoạn nào đề cập đến cái lày)
+    +) Tính cosine similiratity cho đôi một "hàng trong C"  
+    +) Output: Ma trận Rc (size NxN, N là số items trong system)
+
+B3: Tính score tổng hợp từ hai chỉ số trên cho các unseen item 
+(NHỚ LÀ CHỈ CHO CÁC UNSEEN ITEMS NHA)
+    +) Công thức (1) trong paper
+
+
+
+"""
