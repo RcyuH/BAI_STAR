d8e890a695bb5fff1be50bcc5e2ad15db48b6666
RcyuH <phunghuy.ms@gmail.com>
2025-02-21 14:54:59 +0700

    update preprocessing.py

--- STAR_project/preprocessing.py
+++ STAR_project/preprocessing.py
@@ -7,6 +7,7 @@ Created on Sun Feb 16 15:53:44 2025
 """
 
 import pandas as pd
+import numpy as np
 import gzip
 import json
 from item_embedding import ItemEmbeddingGenerator
@@ -33,33 +34,51 @@ class preProcessing_metadata:
     def __init__(self):
         pass
     
-    def extract_6_core(self, items_df: pd.DataFrame) -> pd.DataFrame:
-        # Trích xuất 6 thuộc tính chính: title, description, category, brand, sales ranking, price
-        pass
+    def extract_7_core(self, items_df: pd.DataFrame) -> pd.DataFrame:
+        # Trích xuất 7 thuộc tính chính: title, description, category, brand, sales ranking, price, asin
+        
+        return items_df[['title', 'description', 'category', 'brand', 'rank', 'price', 'asin']] 
     
-    def drop_duplicate(self):
+    def drop_duplicate(self, items_df: pd.DataFrame) -> pd.DataFrame:
         # Xóa các product có id trùng và giữ lại cái đầu tiên
-        pass
+        
+        return items_df.drop_duplicates(subset=["asin"], keep="first")
     
-    def price_processing(self):
-        pass
+    def price_processing(self, items_df: pd.DataFrame) -> pd.DataFrame:
+        items_df['price'] = np.where(items_df['price'].astype(str).str.startswith("$"), items_df['price'], np.nan)
+        
+        return items_df
     
-    def rank_processing(self):
-        pass
+    def rank_processing(self,  items_df: pd.DataFrame) -> pd.DataFrame:  
+        items_df["rank"] = items_df['rank'].str.rstrip('(').str.replace('&amp;', '&', regex=False)
+        
+        return items_df
+    
+    def desc_processing(self,  items_df: pd.DataFrame) -> pd.DataFrame:  
+        items_df['description'] = items_df['description'].str[0]
+        
+        return items_df
     
     def convert_df_to_dict(self, items_df: pd.DataFrame) -> dict:  
         items_dict = items_df.set_index("asin").to_dict(orient="index")
         
         return items_dict
     
-    def processing_flow(self):
-        pass
+    def processing_flow(self, items_df: pd.DataFrame) -> dict:
+        items_df_core = self.extract_7_core(items_df)
+        items_df_clean = self.drop_duplicate(items_df_core)
+        items_df_clean = self.price_processing(items_df_clean)
+        items_df_clean = self.rank_processing(items_df_clean)
+        items_df_clean = self.desc_processing(items_df_clean)
+        items_df_clean = self.convert_df_to_dict(items_df_clean)
+        
+        return items_df_clean   
+
+if __name__ == "__main__":
+    # Ví dụ sử dụng
+    pre = preProcessing_metadata()
+    meta_data_dict = pre.processing_flow(meta_data_df)
     
-meta_data_df_cleaned = meta_data_df.drop_duplicates(subset=["asin"], keep="first")    
-
-pre = preProcessing_metadata()
-meta_data_dict = pre.convert_df_to_dict(meta_data_df_cleaned)
-
-generator = ItemEmbeddingGenerator()
-generator.debug_prompt(meta_data_dict)
+    generator = ItemEmbeddingGenerator()
+    generator.debug_prompt(meta_data_dict)
     
